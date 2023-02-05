701126d6746e54ca534c6ca6f7ec26714719adc2
Rushibhumkar <rushibhumkar11@gmail.com>
2023-02-05 17:22:16 +0530

    update

---
+++ js/script.js
@@ -0,0 +1,13 @@
+// typing Animation
+var typed = new Typed(".typing", {
+  strings: [
+    "Web Design",
+    "Web Developer",
+    "Graphic Designer",
+    "YouTuber",
+    "App Developer",
+  ],
+  typeSpeed: 100,
+  BackSpeed: 60,
+  loop: true,
+});
